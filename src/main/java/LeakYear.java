4412eef8afa51c7746ccd1a7d933fef5b3a833ee
Wojciech Osiadacz <wojciechosiadacz88@gmail.com>
2023-05-10 19:45:34 +0200

    firstcommit

---
+++ src/main/java/LeakYear.java
@@ -0,0 +1,8 @@
+public class LeakYear {
+    public static void main(String[]args){
+        int firstNumber = 365;
+
+
+
+    }
+}
---
+++ src/main/java/MainExtanded.java
@@ -0,0 +1,16 @@
+public class MainExtanded {
+    public static void main(String[]args){
+        String example = "Ura";
+        String example2 = "Bura";
+        String example3 = "Szef";
+        String example4 ="Podwura";
+        String example5 = "Taki";
+        String example6 = "Z niego jest";
+        System.out.println(example);
+        System.out.println(example2);
+        System.out.println(example3);
+        System.out.println(example4);
+        System.out.println(example5);
+        System.out.println(example6);
+    }
+}
